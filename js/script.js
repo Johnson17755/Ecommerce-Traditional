7704cef3cb2b2bb9c084b01776caa30f51b530ab
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-03-27 10:57:40 -0700

    A landing page on an ecommerce that has to do with traditional attire,watch,shoe and material

--- js/script.js
+++ js/script.js
@@ -16,3 +16,31 @@ function resetFilterBtns(){
     $(this).removeClass('active-filter-btn');
   });
 }
+
+
+const body = document.querySelector("body"),
+       modeToggle = document.querySelector(".dark-light");
+
+               // when you refresh a page it store the mode(dark and light)
+               let getMOde = localStorage.getItem("mode");
+               if(getMOde && getMOde === "dark-mode"){
+                   body.classList.add("dark");
+               }
+           
+       
+       
+           // js code to toggle dark and light mode
+               modeToggle.addEventListener("click", () =>{
+               modeToggle.classList.toggle("active");
+               body.classList.toggle("dark");
+       
+       
+       
+               // to keep user select mood even page refersh file reopen
+               if(!body.classList.contains("dark")){
+                   localStorage.setItem("mode" , "light-mode");
+               } else {
+                   localStorage.setItem("mode" , "dark-mode");
+               }
+              });
+       
